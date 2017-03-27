d1474f39ee04001a94a3706913ec5ba1ae95a640
Shannon Luva Thompson <shannont622@gmail.com>
2017-03-26 18:34:28 -0700

    Program that asks user questions about California and tells you how many of your answers were wrong/right

---
+++ js/quiz.js
@@ -0,0 +1,36 @@
+function print(message) {
+    document.write(message);
+}
+
+function californiaQuiz() {
+    var questionsArray = [
+        ["What city is the capital of California?", "sacramento"],
+        ["In what year did California become a state?", 1850],
+        ["What's the largest city in California?", "los angeles"],
+        ["How many million people live in California?", "39"]
+    ];
+    console.log("first item: " + questionsArray[0][0]);
+    var answeredRightMsg = "<h2>You got these questions right:</h2>";
+    var answeredWrongMsg = "<h2>You got these questions wrong:</h2>";
+    var rightAnswers = 0;
+    var wrongAnswers = 0;
+
+    for (var i=0; i < 4; i += 1) {
+        var answer = (prompt(questionsArray[i][0])).toLowerCase();
+        if (answer == questionsArray[i][1]){
+            rightAnswers += 1;
+            answeredRightMsg += "<p>" + rightAnswers + ". " + questionsArray[i][0] + "</p>";
+        } else {
+            wrongAnswers += 1;
+            answeredWrongMsg += "<p>" + wrongAnswers + ". " + questionsArray[i][0] + "</p>";
+        }
+    }
+    document.write("<h2>You got " + rightAnswers + " question(s) correct.</h2>");
+    if (rightAnswers != 0) {
+        print(answeredRightMsg);
+    }
+    if (wrongAnswers != 0) {
+        print(answeredWrongMsg);
+    }
+}
+californiaQuiz();
